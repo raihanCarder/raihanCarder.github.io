e24ad592fa2576b37b4046d2127310a208941cb0
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2026-01-29 17:56:30 -0500

    feat: switch skill section for education section

---
+++ src/components/EducationHeader.tsx
@@ -0,0 +1,14 @@
+export default function EducationHeader() {
+  return (
+    <div className="education-header">
+      <div>
+        <h3>University of Toronto Scarborough</h3>
+        <p className="education-major">HBSc Computer Science (Co-op)</p>
+        <p className="education-specialization">
+          Specialization: Software Engineering
+        </p>
+      </div>
+      <span className="education-grad">Expected Graduation: 2028</span>
+    </div>
+  );
+}
